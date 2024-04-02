723d0a631139ec5053744c2d3e91694fc44cccd1
martin265 <martinsilungwe12@gmail.com>
2024-04-02 16:39:22 +0200

    working on the events page

--- pages/events.py
+++ pages/events.py
@@ -1,1 +1,21 @@
 import flet as ft
+
+
+class EventsPage(ft.Container):
+    def __init__(self, page: ft.Page):
+        super().__init__()
+        self.page = page
+
+        # ============ the controls for the page will be here ========== //
+        self.content = ft.SafeArea(
+            content=ft.Column(
+                controls=[
+                    # =========== the top container for the tabs will be here
+                    ft.Container(
+                        content=ft.Row(
+
+                        )
+                    )
+                ]
+            )
+        )
