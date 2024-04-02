5ff3343576e675ae02062d8ec23aacbebd409498
martin265 <martinsilungwe12@gmail.com>
2024-04-02 17:09:25 +0200

    working on the events page

--- pages/events.py
+++ pages/events.py
@@ -1,10 +1,11 @@
 import flet as ft
-
+from config.config import supabase
 
 class EventsPage(ft.Container):
     def __init__(self, page: ft.Page):
         super().__init__()
         self.page = page
+        self.all_events = ft.Column([])
 
         # ============ the controls for the page will be here ========== //
         self.content = ft.SafeArea(
@@ -31,7 +32,17 @@ class EventsPage(ft.Container):
                                 ),
 
                                 # =============== // the container for the icon will be here ===== //
-
+                                ft.Container(
+                                    content=ft.Row(
+                                        alignment=ft.MainAxisAlignment.CENTER,
+                                        controls=[
+                                            ft.Icon(
+                                                ft.icons.EVENT_AVAILABLE_ROUNDED,
+                                                size=30
+                                            )
+                                        ]
+                                    )
+                                )
 
                             ]
                         )
@@ -39,3 +50,39 @@ class EventsPage(ft.Container):
                 ]
             )
         )
+
+
+    def fetch_all_events(self):
+        try:
+            data, count = supabase.table("Artists").select("*").execute()
+            # =========== checking if the data is available here ======== //
+            if not data:
+                print("no available records")
+            else:
+                data_tuple = data
+                data_list = data_tuple[1]
+                # first_names = [artist['first_name'] for artist in data_list]
+
+                for element in data_list:
+                    self.all_events.controls.append(
+                        ft.Container(
+                            adaptive=True,
+                            content=ft.Column(
+                                controls=[
+
+                                ]
+                            )
+                        )
+                    )
+        except Exception as ex:
+            self.page.snack_bar = ft.SnackBar(
+                content=ft.Row(
+                    controls=[
+                        ft.Text(
+                            'something went wrong at {}'.format(ex)
+                        )
+                    ]
+                )
+            )
+            self.page.snack_bar.open = True
+            self.page.update()
